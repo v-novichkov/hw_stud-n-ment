254990b9a7c55a57bade80a1bea01068d366396b
V. Novichkov <novichkov.store@gmail.com>
2023-01-19 23:09:36 +0300

    All task resolved, check __str_ Student

--- main.py
+++ main.py
@@ -20,6 +20,25 @@ class Student:
         else:
             return 'Ошибка'
 
+    def mid_grade(self):
+        count_courses = 0
+        count_grades = 0
+        sum_grades = 0
+        for k, v  in self.grades.items():
+            if k is not None and v is not None:
+                count_courses += 1
+                count_grades += len(v)
+                sum_grades += sum(v)
+            else:
+                print(f'У студента {self.name} нет закрепленных курсов или оценок')
+                break
+        mid_grades = sum_grades / count_grades / count_courses
+        return mid_grades
+
+    def __str__(self):
+        res = f' Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за дз: {self.mid_grade()} \nКурсы в процессе изучения: {self.courses_in_progress} \nЗавершенные курсы: {self.finished_courses}'
+        return res
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -34,6 +53,24 @@ class Lecturer(Mentor):
       self.grades = {}
 
 
+    def mid_grade(self):
+        count_courses = 0
+        count_grades = 0
+        sum_grades = 0
+        for k, v  in self.grades.items():
+            if k is not None and v is not None:
+                count_courses += 1
+                count_grades += len(v)
+                sum_grades += sum(v)
+            else:
+                print(f'У лектора {self.name} нет закрепленных курсов или оценок')
+        mid_grades = sum_grades / count_grades / count_courses
+        return mid_grades
+
+    def __str__(self):
+        res = f' Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.mid_grade()}'
+        return res
+
 class Reviewer(Mentor):
 
     def rate_hw(self, student, course, grade):
@@ -45,13 +82,31 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        res = f' Имя: {self.name} \n Фамилия: {self.surname}'
+        return res
+
 # для проверки
 Ivan_Ivanov = Student('Ivan', 'Ivanov', 'Male')
 Petr_Petrov = Lecturer('Petr', 'Petrov')
+Petr_Petrovs = Reviewer('Petr', 'Petrovs')
+
+Petr_Petrovs.courses_attached.append('math')
+Petr_Petrovs.courses_attached.append('info')
+Petr_Petrovs.rate_hw(Ivan_Ivanov, 'math', 8)
+Petr_Petrovs.rate_hw(Ivan_Ivanov, 'info', 8)
 
 Petr_Petrov.courses_attached.append('math')
 Ivan_Ivanov.courses_in_progress.append('math')
-
+Petr_Petrov.courses_attached.append('info')
+Ivan_Ivanov.courses_in_progress.append('info')
+Ivan_Ivanov.finished_courses.append('python')
+#
 Ivan_Ivanov.rate_lect(Petr_Petrov, 'math', 1)
+Ivan_Ivanov.rate_lect(Petr_Petrov, 'math', 3)
+Ivan_Ivanov.rate_lect(Petr_Petrov, 'info', 8)
+Ivan_Ivanov.rate_lect(Petr_Petrov, 'info', 1)
 
-print(Petr_Petrov.grades)
+print(Ivan_Ivanov.grades)
+# print(Petr_Petrov)
+# print(Ivan_Ivanov)
